b57658d292f0afecd244a348af1646111a05ff93
Aslm <aslmmon993@gmail.com>
2025-04-06 14:15:30 +0200

    added ui impl for character in ui

--- lib/presentation/ui/providers/gamestateprovider.dart
+++ lib/presentation/ui/providers/gamestateprovider.dart
@@ -1,8 +1,15 @@
 import 'package:endless_surge/data/repositoriesImpl/character_repository_impl.dart'
     show CharacterRepositoryImpl;
+import 'package:endless_surge/domain/entities/character.dart' show Character;
 import 'package:endless_surge/domain/repositories/character_repository.dart'
     show CharacterRepository;
 import 'package:riverpod/riverpod.dart';
 
-final characterRepositoryProvider = Provider<CharacterRepository>((ref) => CharacterRepositoryImpl(),
+final characterRepositoryProvider = Provider<CharacterRepository>(
+  (ref) => CharacterRepositoryImpl(),
 );
+
+final characterProvider = StateProvider<Character>((ref) {
+  final characterRepository = ref.watch(characterRepositoryProvider);
+  return characterRepository.getCharacter();
+});
